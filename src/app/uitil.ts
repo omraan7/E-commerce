ca0d27603e0fcd7c2b0e0dd53af2aeefa428e584
omraan7 <mohammedommran@gmail.com>
2026-04-09 16:02:01 +0200

    NextAuthToken

--- src/app/uitil.ts
+++ src/app/uitil.ts
@@ -25,8 +25,9 @@ interface MyJWT {
 
 export async function getRealToken(): Promise<string | null> {
   const cookie =  await cookies()
-const NextAuthToken = cookie.get("next-auth.session-token")?.value 
-
+  const NextAuthToken = 
+    cookie.get("__Secure-next-auth.session-token")?.value ??
+    cookie.get("next-auth.session-token")?.value;
   if (!NextAuthToken) return null;
 
   const decoded = await decode({ secret: process.env.NEXTAUTH_SECRET!, token: NextAuthToken }) as MyJWT | null;
